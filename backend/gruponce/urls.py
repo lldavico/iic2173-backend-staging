d878b86fa7c38ec4efb128d49040bfb0453a85a7
Vicente Aguilera <vjaguilera@uc.cl>
2020-09-30 09:29:23 -0300

    Fix breaked thread endpoint

--- backend/gruponce/urls.py
+++ backend/gruponce/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path(r"boards/", include("gruponce.board.urls"), name="boards"),
+    path(r"threads/", include("gruponce.thread.urls"), name="threads"),
 ]
