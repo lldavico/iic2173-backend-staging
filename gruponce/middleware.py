608ae825db6dc8d27e04e9eb33203e39fa627091
Luciano <lldavico@uc.cl>
2020-10-13 00:28:06 -0300

    add aws info to header

--- gruponce/middleware.py
+++ gruponce/middleware.py
@@ -1,9 +1,11 @@
+from django.conf.settings import AWS_EC2_DNS, AWS_EC2_IP 
+
 def AwsEC2Middleware(get_response):
 
     def middleware(request):
         response = get_response(request)
-        response['EC2-Instance-IP'] = request.META.get('REMOTE_ADDR')
-        response['X-Forwarder-For'] = request.META.get('HTTP_X_FORWARDED_FOR')
+        response['EC2-Instance-IP'] = AWS_EC2_IP
+        response['EC2-Instance-DNS'] = AWS_EC2_DNS
         return response
 
     return middleware
