5ad2b2d1bdb98eb44a556341df9b83f3d3c212f6
Luciano <lldavico@uc.cl>
2020-11-08 18:55:29 -0300

    pls

--- ci-scripts/start.sh
+++ ci-scripts/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 sudo apt update
-cd e1
-cd backend-project
 sudo docker-compose -f docker-compose.test.yml down --remove-orphans
 sudo docker-compose -f docker-compose.test.yml build
 sudo docker-compose -f docker-compose.test.yml up -d
