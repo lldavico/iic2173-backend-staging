20809e5ce7b9888b6d505e402c07b6b3a724caf8
Luciano Davico <lldavico@uc.cl>
2020-11-10 11:49:04 -0300

    v0.1.2 updated scripts for codeploy

--- ci-scripts/start.sh
+++ ci-scripts/start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo apt update
+sudo docker-compose -f /home/ubuntu/e1/backend-project/docker-compose.test.yml run -d api python manage.py makemigrations
+sudo docker-compose -f /home/ubuntu/e1/backend-project/docker-compose.test.yml run -d api python manage.py migrate
 sudo docker-compose -f /home/ubuntu/e1/backend-project/docker-compose.test.yml up -d
